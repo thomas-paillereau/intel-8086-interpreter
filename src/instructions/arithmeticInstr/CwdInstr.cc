c4f150b2bf7fc7fd22de8e4df31b50c1068b263f
thomas <thomas.paillereau@epita.fr>
2026-08-25 23:52:59 +0200

    add: CWD execution

--- src/instructions/arithmeticInstr/CwdInstr.cc
+++ src/instructions/arithmeticInstr/CwdInstr.cc
@@ -4,3 +4,17 @@ CwdInstr::CwdInstr([[maybe_unused]] const std::vector<uint8_t> &content, int pos
     name_ = "cwd";
     position_ = position;
 }
+
+bool CwdInstr::execute(Cpu &cpu, bool &halt, [[maybe_unused]] bool printing) {
+    if (!halt) {
+        if ((cpu.getReg16(Cpu::AX) & 0x8000) != 0) {
+            cpu.setReg16(Cpu::DX, 0xFFFF);
+        } else {
+            cpu.setReg16(Cpu::DX, 0x0000);
+        }
+    }
+    cpu.addToIp(size_);
+    if (position_ + size_ <= position_)
+        return true;
+    return false;
+}
